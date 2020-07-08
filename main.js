a222e0747a526fb86ce0b197ee3afcb2a7d889ce
Bartek Piotrowicz <barewicz87@gmail.com>
2020-07-08 20:51:56 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('siema');
+const name = 'Bartek';
+const age = 31;
+console.log(name);
+console.log(age);
+console.log(`${name}
+${age}`);
+
+console.log(`Nazywam się ${name} 
+i mam ${age} lat`);
+console.log('Siemanko, nie zgubiłeś się?? 😎😎😎')
